73936964eb06d911b6ac3bfdc502db4309480ecb
P.placht <philip.placht@gmx.at>
2026-05-19 14:46:10 +0200

    data

---
+++ CS/SEWCS/SEWCS5.19/Functions.cs
@@ -0,0 +1,17 @@
+﻿namespace SEWCS5._19;
+
+public class Functions
+{
+    public static int Power(int a, int exp)
+    {
+        if (exp <= 0) return 1;
+        return a * Power(a, exp - 1);
+    }
+
+    public static int EvenCount(List<int> list, int index)
+    {
+        if (index >= list.Count) return 0;
+        int value = list[index] % 2 == 0 ? 1 : 0;
+        return value + EvenCount(list, index + 1);
+    }                                                  
+}
---
+++ CS/SEWCS/SEWCS5.19/Program.cs
@@ -0,0 +1,5 @@
+﻿using SEWCS5._19;
+
+List<int> list = new List<int> {2,3,4, 4};
+
+Console.WriteLine(Functions.EvenCount(list, 0));
